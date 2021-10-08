a44b2d22fdd35b0ac0aa92c750f28f711aec806a
ddulesov <dmitry.dulesov@gmail.com>
2021-10-08 20:12:39 +0300

    fix: add comments for common_lido_test.py and fix a typo

--- tests/common_lido_test.py
+++ tests/common_lido_test.py
@@ -7,7 +7,7 @@ UNIT = 1_000_000_000
 def test_fee_distribution(vKSM, LedgerMock, mocklido, mockledger, treasury, developers, admin):
     '''
     Use default fee distribution:
-    total 10% fee spleets between operators 3%, treasury 5.6%  and developers 1.4%
+    total 10% fee splits between operators 3%, treasury 5.6%  and developers 1.4%
     '''
     assert mocklido.balanceOf(admin) == 0
     vKSM.approve(mocklido, 10*UNIT, {'from': admin})
@@ -20,6 +20,7 @@ def test_fee_distribution(vKSM, LedgerMock, mocklido, mockledger, treasury, deve
 
     assert mocklido.getFee() == 1000
     # call lido.distributeRewards via mock Ledger
+    # 1 UNIT has already withdrawn operators (3%) fee
     t = mockledger.distributeRewards(1*UNIT, {'from': admin})
     print(t.info())
 
@@ -40,7 +41,7 @@ def test_fee_change_distribution(vKSM, LedgerMock, mocklido, mockledger, treasur
     assert mocklido.balanceOf(admin) == 10 * UNIT
     # call lido.distributeRewards via mock Ledger
     mocklido.setFee(300, 0, 700)
-    # assert mocklido.FEE_BP() == 0x0
+    assert mocklido.getFee() == 1000
 
     assert mocklido.balanceOf(treasury) == 0
     t = mockledger.distributeRewards(1*UNIT, {'from': admin})
@@ -52,6 +53,7 @@ def test_fee_change_distribution(vKSM, LedgerMock, mocklido, mockledger, treasur
 
     mocklido.setFee(300, 700, 0)
     # call lido.distributeRewards via mock Ledger
+    # 1 UNIT has already withdrawn operators (3%) fee
     mockledger.distributeRewards(1*UNIT, {'from': admin})
 
     assert mocklido.balanceOf(treasury) == 72_164_946    # ~72.1 MUNIT (7% gives 7.21% of 97%)
