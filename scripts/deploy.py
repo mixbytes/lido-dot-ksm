76a10efa5f223c4c613f26794802b8fb9bb188e1
Algys Ievlev <algys.ievlev@gmail.com>
2021-11-17 17:01:53 +0300

    feat: refactored deployment script and added config

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -4,7 +4,15 @@ from pathlib import Path
 from brownie import *
 import base58
 from hashlib import blake2b
+import json
+import yaml
+from pathlib import Path
+from colorama import Fore, Back, Style, init
 
+init(autoreset=True)
+
+
+NETWORK="moonbase"
 
 def get_derivative_account(root_account, index):
     seed_bytes = b'modlpy/utilisuba'
@@ -23,7 +31,29 @@ if hasattr(project, 'OpenzeppelinContracts410Project'):
 else:
     OpenzeppelinContractsProject = project.OpenzeppelinContractsProject
 
-LiquidstakingOracleProject = project.LidoDotKsmProject
+
+def load_deployments(network):
+    path = "./deployments/" + network + '.json'
+    if Path(path).is_file():
+        with open(path) as file:
+            return json.load(file)
+    else:
+        return {}
+
+
+def save_deployments(deployments, network):
+    path = "./deployments/" + network + '.json'
+    with open(path, 'w') as file:
+        json.dump(deployments, file)
+
+
+def load_deployment_config(network):
+    with open('./deployment-config.yml') as file:
+        return yaml.safe_load(file)['networks'][network]
+
+
+CONFIG = load_deployment_config(NETWORK)
+DEPLOYMENTS = load_deployments(NETWORK)
 
 
 # global configs
@@ -32,49 +62,62 @@ GAS_PRICE = "3 gwei"
 GAS_LIMIT = 10*10**6
 
 
-# oracles
-ALL_ROLES = ['ROLE_SPEC_MANAGER',
-             'ROLE_PAUSE_MANAGER',
-             'ROLE_FEE_MANAGER',
-             'ROLE_ORACLE_MANAGER',
-             'ROLE_LEDGER_MANAGER',
-             'ROLE_STAKE_MANAGER',
-             'ROLE_ORACLE_MEMBERS_MANAGER',
-             'ROLE_ORACLE_QUORUM_MANAGER']
-
-
-# stashes
-ROOT_DERIVATIVE = '5HYQeRamG9nQyRYAUoyHMuZ9tnSTPNSyim4uCBXuBCHJJm6a'
-STASH_IDX = [40, 41, 42]
-STASH = [ get_derivative_account(ROOT_DERIVATIVE, idx) for idx in STASH_IDX ]
-
-
-# Oracles
-ORACLES = [accounts.add(private_key='0x925eda0e60dac4a29712e1f9cfe1a3f1efe4270596e46722295248428f25e6ee'), accounts.add(private_key='0x0801d35e1dbb9e47f89ff7971c627617eef53ced08e622e82bc551540efdcb4d')]
-QUORUM = 2
-
-
-# contracts/precompiles/deployer
-DEPLOYER = accounts.from_mnemonic("rough spider regular borrow shrimp noble scare strike color goddess diesel laugh")
-PROXY_ADMIN = OpenzeppelinContractsProject.ProxyAdmin.at('0x18d5d0ae23C60E109d88F9E92068c307c02D1fdd', owner=DEPLOYER)
-VKSM = interface.IERC20('0xFFFFFFFF1FCACBD218EDC0EBA20FC2308C778080')
-CONTROLLER = Controller.at('0xD52E642Fc8ddabEb803F1382970a3c13822ca47e')
-
-#xcmTransactor = interface.IXcmTransactor('0x0000000000000000000000000000000000000806')
-#relayEncoder = interface.IRelayEncoder('0x0000000000000000000000000000000000000805')
-#xtoken = interface.IxTokens('0x0000000000000000000000000000000000000804')
-
 
 # utils
 def ss58decode(address):
     return Keypair(ss58_address=address).public_key
 
 
-def get_opts(sender=DEPLOYER, gas_price=GAS_PRICE, gas_limit=GAS_LIMIT):
+def get_opts(sender, gas_price=GAS_PRICE, gas_limit=GAS_LIMIT):
     return {'from': sender, 'gas_price': gas_price, 'gas_limit': gas_limit}
 
 
+def get_deployment(container):
+    info = container.get_verification_info()
+    name = info['contract_name']
+    if name in DEPLOYMENTS:
+        return DEPLOYMENTS[name]
+    else:
+        return None
+
+
+def add_new_deploy(container, address):
+    info = container.get_verification_info()
+    name = info['contract_name']
+    DEPLOYMENTS[name] = address
+    save_deployments(DEPLOYMENTS, NETWORK)
+
+
+def yes_or_no(question):
+    reply = input(question+' (y/n): ').lower().strip()
+    if reply[0] == 'y':
+        return True
+    if reply[0] == 'n':
+        return False
+    else:
+        return yes_or_no(Fore.RED + "Uhhhh... please enter y/n ")
+
+def check_and_get_deployment(container):
+    deployment = get_deployment(container)
+    name = container.get_verification_info()["contract_name"]
+    if deployment:
+        if yes_or_no(Fore.RED + f'Found old deployment for {name} at {deployment}, use it?'):
+            return container.at(deployment)
+        else:
+            print(Fore.RED + f'REDEPLOYING {name} contract to new address')
+    return None
+
+
 def deploy_with_proxy(container, proxy_admin, deployer, *args):
+    print("")
+
+    deployment = check_and_get_deployment(container)
+    if deployment:
+        return deployment
+
+    name = container.get_verification_info()["contract_name"]
+    print(Fore.GREEN + f'DEPLOYING {name} ...')
+
     owner = proxy_admin.owner()
     _implementation = container.deploy(get_opts(deployer))
     encoded_inputs = _implementation.initialize.encode_input(*args)
@@ -87,116 +130,140 @@ def deploy_with_proxy(container, proxy_admin, deployer, *args):
     )
     OpenzeppelinContractsProject.TransparentUpgradeableProxy.remove(_instance)
 
+    add_new_deploy(container, _instance.address)
+    print(Fore.GREEN + f'Contract {name} deployed at {Fore.YELLOW}{_instance.address} {Fore.GREEN}under {Fore.RED} proxy')
     return container.at(_instance.address)
+
+
+def deploy(container, deployer, *args):
+    print("")
+
+    deployment = check_and_get_deployment(container)
+    if deployment:
+        return deployment
+
+    name = container.get_verification_info()["contract_name"]
+    print(Fore.GREEN + f'DEPLOYING {name} ...')
+
+    inst = None
+    if args:
+        inst = container.deploy(*args, get_opts(deployer))
+    else:
+        inst = container.deploy(get_opts(deployer))
+
+    add_new_deploy(container, inst.address)
+    print(Fore.GREEN + f'Contract {name} deployed at {Fore.YELLOW}{inst.address}')
+
+    return inst
 
 
 # deploy functions
 def deploy_proxy_admin(deployer):
-    ProxyAdmin = OpenzeppelinContractsProject.ProxyAdmin
-    return ProxyAdmin.deploy(get_opts(deployer))
+    return deploy(OpenzeppelinContractsProject.ProxyAdmin, deployer)
 
 
-def deploy_auth_manager(deployer, admin, auth_super_admin):
-    return deploy_with_proxy(AuthManager, admin, deployer, auth_super_admin)
+def deploy_auth_manager(deployer, proxy_admin, auth_super_admin):
+    return deploy_with_proxy(AuthManager, proxy_admin, deployer, auth_super_admin)
 
 
 def deploy_oracle_clone(deployer):
-    return Oracle.deploy(get_opts(deployer))
+    return deploy(Oracle, deployer)
 
 
 def deploy_oracle_master(deployer):
-    return OracleMaster.deploy(get_opts(deployer))
+    return deploy(OracleMaster, deployer)
 
 
 def deploy_ledger_clone(deployer):
-    return Ledger.deploy(get_opts(deployer))
+    return deploy(Ledger, deployer)
 
 
-def deploy_lido(deployer, admin, auth_manager, vksm, controller, treasury, developers):
-    return deploy_with_proxy(Lido, admin, deployer, auth_manager, vksm, controller, treasury, developers)
+def deploy_controller(deployer, proxy_admin):
+    return deploy_with_proxy(Controller, proxy_admin, deployer)
+
+
+def deploy_lido(deployer, proxy_admin, auth_manager, vksm, controller, treasury, developers):
+    return deploy_with_proxy(Lido, proxy_admin, deployer, auth_manager, vksm, controller, treasury, developers)
 
 
 # deployment
 def main():
-    deployer = DEPLOYER
-    admin = DEPLOYER
-    auth_super_admin = DEPLOYER
-    treasury = DEPLOYER
-    developers = DEPLOYER
-    roles = {
-        'ROLE_SPEC_MANAGER': DEPLOYER,
-        'ROLE_PAUSE_MANAGER': DEPLOYER,
-        'ROLE_FEE_MANAGER': DEPLOYER,
-        'ROLE_ORACLE_MANAGER': DEPLOYER,
-        'ROLE_LEDGER_MANAGER': DEPLOYER,
-        'ROLE_STAKE_MANAGER': DEPLOYER,
-        'ROLE_ORACLE_MEMBERS_MANAGER': DEPLOYER,
-        'ROLE_ORACLE_QUORUM_MANAGER': DEPLOYER
-    }
-    oracles = ORACLES
-    oracle_quorum = QUORUM
-    vksm = VKSM.address
-    controller = CONTROLLER.address
-    era_sec = 21600 # moonbase
-    max_validators_per_ledger = 16
-    min_moninator_balance = 1 * 10**12
-    stashes = STASH
-    stash_idxs = STASH_IDX
+    deployer = accounts.at(CONFIG['deployer'])
 
-    proxy_admin = PROXY_ADMIN
-    print("Proxy admin:", proxy_admin)
+    auth_super_admin = CONFIG['auth_sudo']
+    treasury = CONFIG['treasury']
+    developers = CONFIG['developers']
 
-#    print("Deploying auth manager...")
-#    auth_manager = deploy_auth_manager(deployer, proxy_admin, auth_super_admin)
-#    print("Auth manager:", auth_manager)
+    roles = CONFIG['roles']
+    oracles = CONFIG['oracles']
+    oracle_quorum = CONFIG['quorum']
+    vksm = CONFIG['precompiles']['vksm']
+    xcm_transactor = CONFIG['precompiles']['xcm_transactor']
+    relay_encoder = CONFIG['precompiles']['relay_encoder']
+    x_token = CONFIG['precompiles']['x_token']
 
-#    for role in roles:
-#        print("Setting role:", role)
-#        auth_manager.addByString(role, roles[role], get_opts(deployer))
+    era_sec = CONFIG['relay_spec']['era_duratation']
+    max_validators_per_ledger = CONFIG['relay_spec']['max_validators_per_ledger']
+    min_nominator_bond = CONFIG['relay_spec']['min_nominator_bond']
 
-    auth_manager = AuthManager.at('0x06A191588c9FFCd323ad99bcED86efDab3331BBc')
-    print("Deploying lido...")
+    root_derivative_index = CONFIG['root_derivative_index']
+    root_derivative_account = ss58decode(get_derivative_account(CONFIG['sovereign_account'], root_derivative_index))
+    stash_idxs = CONFIG['stash_indexes']
+    stashes = [ss58decode(get_derivative_account(root_derivative_account, idx)) for idx in stash_idxs]
+
+    xcm_max_weight = CONFIG['xcm_max_weight']
+    xcm_weights = CONFIG['xcm_weights']
+
+
+    proxy_admin = deploy_proxy_admin(deployer)
+
+    controller = deploy_controller(deployer, proxy_admin)
+
+    print(f"\n{Fore.GREEN}Configuring controller...")
+    controller.init(root_derivative_index, root_derivative_account, vksm, relay_encoder, xcm_transactor, x_token, get_opts(deployer))
+    controller.setMaxWeight(xcm_max_weight, get_opts(deployer))
+    controller.setWeights([w | (1<<65) for w in xcm_weights], get_opts(deployer))
+
+    auth_manager = deploy_auth_manager(deployer, proxy_admin, auth_super_admin)
+
+    for role in roles:
+        print(f"{Fore.GREEN}Setting role: {role}")
+        if auth_manager.has(web3.solidityKeccak(["string"], [role]), roles[role]):
+            print(f"{Fore.YELLOW}Role {role} already setted, skipping..")
+        else:
+            auth_manager.addByString(role, roles[role], get_opts(deployer))
+
     lido = deploy_lido(deployer, proxy_admin, auth_manager, vksm, controller, treasury, developers)
-    #lido = Lido.at('0xEBe2Aeaf123922C6f7B5cc98b8a4FD172c6eD92b')
-    print('Lido:', lido)
 
-
-    print("Deploying oracle clone...")
     oracle_clone = deploy_oracle_clone(deployer)
-    print('Oracle clone:', oracle_clone)
 
-    print("Deploying oracle master...")
     oracle_master = deploy_oracle_master(deployer)
-#    oracle_master = OracleMaster.at('0x70ED8c2786718146b681dfd91796C288C0274C06')
-    print('Oracle master:', oracle_master)
 
-    print('Oracle master initialization...')
     oracle_master.initialize(oracle_clone, oracle_quorum, get_opts(deployer))
 
-    print("Deploying ledger clone...")
     ledger_clone = deploy_ledger_clone(deployer)
-#    ledger_clone = Ledger.at('0xe3B75E296B8Bf8eEbFa72849D0Ce3Bf942e6C3Eb')
-    print('Ledger clone:', ledger_clone)
 
-    print('Lido configuration...')
+    print(f'\n{Fore.GREEN}Lido configuration...')
     lido.setOracleMaster(oracle_master, get_opts(roles['ROLE_ORACLE_MANAGER']))
     lido.setLedgerClone(ledger_clone, get_opts(roles['ROLE_ORACLE_MANAGER']))
-    lido.setRelaySpec((1, era_sec, era_sec * (28+3), max_validators_per_ledger, min_moninator_balance), get_opts(roles['ROLE_SPEC_MANAGER']))
+    lido.setRelaySpec((1, era_sec, era_sec * (28+3), max_validators_per_ledger, min_nominator_bond), get_opts(roles['ROLE_SPEC_MANAGER']))
 
+    print(f'\n{Fore.GREEN}Adding oracle members...')
     for oracle in ORACLES:
-        print("Adding oracle member:", oracle)
+        print(f"{Fore.YELLOW}Adding oracle member: {oracle}")
         oracle_master.addOracleMember(oracle, get_opts(roles['ROLE_ORACLE_MEMBERS_MANAGER']))
 
     ledgers = []
+    print(f'\n{Fore.GREEN}Adding ledgers...')
     for i in range(len(stashes)):
         s_bytes = ss58decode(stashes[i])
-        print("Adding ledger, stash:", stashes[i], stash_idxs[i])
+        print(f"{Fore.GREEN}Added ledger, idx: {stash_idxs[i]} stash: {stashes[i]}")
         lido.addLedger(s_bytes, s_bytes, stash_idxs[i], 100, get_opts(roles['ROLE_LEDGER_MANAGER']))
         ledgers.append(lido.findLedger(s_bytes))
 
 #    for ledger in ledgers:
 #        print("Refreshing allowances for ledger:", ledger)
-#        Ledger.at(ledger).refreshAllowances(get_opts())
+#        Ledger.at(ledger).refreshAllowances(get_opts(deployer))
 #
 #    print("Refreshing allowances for lido")
 #    lido.refreshAllowances(get_opts(roles['ROLE_LEDGER_MANAGER']));
